0cb339ba8e76df6bc129bd86dee85487c488750f
LENOVO <70053319@elp.edu.pe>
2023-05-05 17:41:37 -0500

    Persona Clase Controller DTOpersona

--- src/main/java/tutor/ship/com/demo/model/Persona.java
+++ src/main/java/tutor/ship/com/demo/model/Persona.java
@@ -1,4 +1,24 @@
 package tutor.ship.com.demo.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class Persona {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @OneToMany
+    @JoinColumn(name ="tipopersona_id" , referencedColumnName = "tipopersona_id")
+    private Integer tipopersona_id;
+
+    private String nombresApellidos;
+    private String correo;
+    private String matricula;
+    private String usuario;
+    private String password;
+    private String estado;
 }
