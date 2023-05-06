3714dc72a51ec73f392c49175af52fede306f053
Jose <joseabrahanquispeaquino@gmail.com>
2023-05-06 14:36:36 -0500

    agregando CursosDTO,

--- src/main/java/tutor/ship/com/demo/controller/dto/CursosDTO.java
+++ src/main/java/tutor/ship/com/demo/controller/dto/CursosDTO.java
@@ -1,4 +1,16 @@
 package tutor.ship.com.demo.controller.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class CursosDTO {
+    private Integer id;
+    @NotBlank
+    private String denominacion;
+    @NotBlank
+    private String codigo;
+    @NotBlank
+    private Integer persona_id;
+    @NotBlank
+    private String estado;
+
 }
