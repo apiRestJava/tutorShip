a7fe97d219f5aaa151d51ed4ce5b678f63ace916
elpestudio <70147630@elp.edu.pe>
2023-05-06 14:14:01 -0500

    Agregando DTO de cronograma y detalle curso

--- src/main/java/tutor/ship/com/demo/controller/dto/CronogramaCursoDTO.java
+++ src/main/java/tutor/ship/com/demo/controller/dto/CronogramaCursoDTO.java
@@ -1,4 +1,24 @@
 package tutor.ship.com.demo.controller.dto;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import tutor.ship.com.demo.model.Cursos;
+import tutor.ship.com.demo.model.Persona;
+
+import java.sql.Date;
+import java.sql.Time;
+
 public class CronogramaCursoDTO {
+    @NotBlank
+    private Persona docente_id;
+    @NotBlank
+    private Persona tutor_id;
+    @NotBlank
+    private Time horaInicio;
+    @NotBlank
+    private Time horaFin;
+    @NotBlank
+    private Date fecha;
+    @NotBlank
+    private String estado;
 }
--- src/main/java/tutor/ship/com/demo/controller/dto/DetalleCursoPersonaDTO.java
+++ src/main/java/tutor/ship/com/demo/controller/dto/DetalleCursoPersonaDTO.java
@@ -1,4 +1,21 @@
 package tutor.ship.com.demo.controller.dto;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import tutor.ship.com.demo.model.CronogramaCurso;
+import tutor.ship.com.demo.model.Persona;
+
+import java.sql.Date;
+
 public class DetalleCursoPersonaDTO {
+    @NotBlank
+    private Persona alumno_id;
+    @NotBlank
+    private CronogramaCurso cronograma_curso_id;
+    @NotBlank
+    private Persona tutor_id;
+    @NotBlank
+    private Date fecha;
+    @NotBlank
+    private String estado;
 }
