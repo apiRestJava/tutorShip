b99ead435d609a598307126ec3ca660708645324
Jose <joseabrahanquispeaquino@gmail.com>
2023-05-07 13:02:48 -0500

    corriguiendo curso controlador

--- src/main/java/tutor/ship/com/demo/controller/dto/CursosDTO.java
+++ src/main/java/tutor/ship/com/demo/controller/dto/CursosDTO.java
@@ -1,16 +1,16 @@
 package tutor.ship.com.demo.controller.dto;
 
 import jakarta.validation.constraints.NotBlank;
-
+import lombok.Getter;
+import lombok.Setter;
+import tutor.ship.com.demo.model.Personas;
+@Getter
+@Setter
 public class CursosDTO {
-    private Integer id;
-    @NotBlank
+
     private String denominacion;
-    @NotBlank
     private String codigo;
-    @NotBlank
-    private Integer persona_id;
-    @NotBlank
+    private Personas persona_id;
     private String estado;
 
 }
