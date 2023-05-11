ee3377b926729ce4879b474f0bde34535662a8b3
elpestudio <70147630@elp.edu.pe>
2023-05-10 19:32:32 -0500

    Controller modelo, repositorio, dto detalle curso persona

--- src/main/java/tutor/ship/com/demo/controller/dto/DetalleCursoPersonaDTO.java
+++ src/main/java/tutor/ship/com/demo/controller/dto/DetalleCursoPersonaDTO.java
@@ -1,20 +1,23 @@
 package tutor.ship.com.demo.controller.dto;
 
-import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 import tutor.ship.com.demo.model.CronogramaCurso;
 import tutor.ship.com.demo.model.Personas;
 
 import java.sql.Date;
 
+@Getter
+@Setter
 public class DetalleCursoPersonaDTO {
-    @NotBlank
+
     private Personas alumno_id;
-    @NotBlank
+
     private CronogramaCurso cronograma_curso_id;
-    @NotBlank
+
     private Personas tutor_id;
-    @NotBlank
+
     private Date fecha;
-    @NotBlank
+
     private String estado;
 }
