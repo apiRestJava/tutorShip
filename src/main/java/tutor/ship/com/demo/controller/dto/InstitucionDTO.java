da98b8141dd725efa89f555fd78ed75487330cf2
Jose <joseabrahanquispeaquino@gmail.com>
2023-05-12 18:36:25 -0500

    agregando tabla institucionDTO

---
+++ src/main/java/tutor/ship/com/demo/controller/dto/InstitucionDTO.java
@@ -0,0 +1,14 @@
+package tutor.ship.com.demo.controller.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class InstitucionDTO {
+private String nombre;
+private String direccion;
+private String correo;
+private  String celular;
+private String  estado;
+}
